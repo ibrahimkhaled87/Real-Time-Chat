61072c421c93f9ec10fd001cf36cbc1a52244b19
Mona Lisa <ibkhaled87@gmail.com>
2026-05-27 23:51:06 +0300

    cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,13 @@ const app = express();
 // Global Middleware
 app.use(express.json());
 app.use(bodyparser.urlencoded({extended: true}));
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://real-time-chat-kohl-beta.vercel.app"
+  ],
+  credentials: true
+}));
 app.use("/uploads", express.static("uploads"));
 
 // Routes
