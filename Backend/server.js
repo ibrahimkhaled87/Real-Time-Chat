1bfb0b31db5779b8d67fb6db96f6a9425a0025ef
Mona Lisa <ibkhaled87@gmail.com>
2026-05-28 17:31:44 +0300

    backend server cors

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,13 @@ const port = 5000;
 
 const server = http.createServer(app); //Convert app to http server
 const io = new Server(server, {     //Create socket.io server => attach to http server
-    cors: ""
+    cors: {
+        origin: [
+            "http://localhost:3000",
+            "https://real-time-chat-kohl-beta.vercel.app",
+        ],
+        credentials: true
+    }
 })
 socketHandler(io); //Pass socket.io instance to logic 
 app.set("io", io); //To use socket in controller
